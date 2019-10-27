6405cedfad6bb5c0524b698bdce472e10fd587ee
thorpe85 <theoblanchard37@gmail.com>
2019-10-27 15:45:20 +0100

    Add files via upload

---
+++ Shutdown_VM1.ps1
@@ -0,0 +1,9 @@
+﻿$myvarLoaded = Get-PSSnapin -Name NutanixCmdletsPSSnapin -ErrorAction SilentlyContinue | % {$_.Name}
+if ($myvarLoaded -eq $null){Add-PSSnapin NutanixCmdletsPSSnapin}
+#Cluster Credentials
+$ipAddress = "192.168.1.10"
+$clusterUserName = "admin"
+$Secure_String_Pwd = ConvertTo-SecureString "nutanix/4u" -AsPlainText -Force
+Connect-NutanixCluster -Server $ipAddress -UserName $clusterUserName  -Password $Secure_String_Pwd -AcceptInvalidSSLCerts -ForcedConnection
+$vm = Get-NTNXVM -SearchString "my-VM" 
+Set-NTNXVMPowerOff -Vmid $vm.vmId
